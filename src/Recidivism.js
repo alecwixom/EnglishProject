116d18602073311b32997a5838cbfd1b01d68acf
Alec Wixom <atwixom@gmail.com>
2024-10-04 22:28:46 -0600

    update

---
+++ src/Recidivism.js
@@ -0,0 +1,50 @@
+import '../src/Recidivism.css'
+
+function Recidivism() {
+    return (
+        <div className='Recidivism'>
+            <div className='Title'>Alec Wixom
+                <br />
+                ENG- 2010-X07-X08
+                <br />
+                30 September, 2024
+            </div>
+            <div className='body'>
+                <p className='bodytitle'>
+                    Breaking the Cycle of Repeat Offending: How do you break the cycle?
+                </p>
+                <p>
+                    Repeat offenders are a devastating issue in the United States. Almost 80% of our current convicts are repeat offenders (Bureau of Justice). A justice system with an 80% recidivism rate isn’t just a coincidence, it’s an important issue that needs to be addressed. If something isn’t done soon we will see increases in recidivism rates.
+                </p>
+                <p>
+                    Understanding why this is a critical flaw is important, there isn’t just one simple fix for breaking the cycle. Many issues as a collective are putting convicts right back into our justice system. The most commonly arrested repeat offenders are homeless, mentally ill, or have a drug addiction. These commonly arrested individuals have their problems and solutions that vary in execution and cost. The problem is how they are executed and the will of the convict to really strive to break the cycle. With most issues, even ones not listed, this starts with the offender taking the initiative to want help or wanting to break the cycle they are in. Sometimes that’s not enough for them to break the cycle and they need supporting rehabilitation, halfway homes, psychological help. Nancy La Vigne the Director of the National Institute of Justice said this in a statement, “People are sentenced to prison to be removed from society, where ideally, they are rehabilitated so that they lead productive and law-abiding lives upon their release. Unfortunately, prison environments are not always conducive to this goal” (Vigne, 2024, 1). This statement is backed up by the countless interviews with convicts and rehabilitation workers who see it from a first-person perspective. Oftentimes in prisons and jails, they have access to drugs and have un-prevented violence causing longer sentences when caught. Inmates have no choice in those situations of an assault, defending themselves they could be convicted of a crime.
+                </p>
+                <p>
+                    The struggle for someone who is homeless and consistently being arrested is the need for shelter, homeless camps and shelters made for the homeless sometimes have more risk than reward leading them to commit minor offenses so they can stay in a safer environment for a limited period of time. As Nancy stated, sometimes unfortunately the prison environments are able to help as expected. Shelters made temporarily were able to help those in need, much like rehabilitation for drug abuse. This is not to take credit away from the facilities helping those in need, but rather to point out the flaws in having such a high demand for help. There just aren’t enough people that have the time to individually get some people the help they need. When that issue occurs we have a cycle of committing the same crimes and ending right back in the justice system. New programs within the justice system are needed such as the Promise Initiative used by South Carolina. South Carolina’s Political Reporter Avery Wilks stated in his report, “ The state is the second – behind Connecticut – to adopt the Vera Institute of Justice’s Restoring Promise Initiative, a program that uses mentors, classes and community meetings in an effort to change the prison experience so that inmates don’t continue a life of crime after their release”(Wilks,  2019,1).
+                </p>
+                <p>
+                    Introducing new programs such as the Promise Initiative are exactly what the rest of the nation needs in our Justice System. Inmates looking to make a difference and change their lives around will have that opportunity to take classes with mentors to help learn new skills and acclimate to society when they get out. Having a program that teaches a skill to inmates for future employment opportunities and the community meetings to discuss the goals they have, the plans they have for that day and resolving issues non-violently. The program is targeted for younger incarcerated individuals between the ages of 18-25 since they have the highest rate of recidivism and in prison violent offenses. With a program that is as thought out as this, they need to execute it correctly. South Carolina prison in Bishopville is going to start with 12 inmates at a time in this program to see the results, later expanding. I believe that the Restoring Promise Program can help a lot especially with the younger inmates. If they are able to keep the younger generations out of the correctional system, we may be able to see a decline in recidivism over the next few years. They will need to be cautious of how many inmates too mentors they have since other programs such as the state funded/run programs tend to be outnumbered mentors, leading to less of a connection between the staff and convict. The “command and control method”(Vigne, 2024,1), doesn’t work well especially in a reform setting. The climate of tension between the prisoners and guards is already extremely high in the correctional facilities.
+                </p>
+                <p>
+                    You may be wondering why the government programs aren’t good enough, or why the effectiveness of said programs is under question. Well the former prisoners face many challenges after being released, including finding employment, housing, or receiving healthcare. The reform programs are only able to do so much for the ones who gave it their all and really tried to do better. Once they are released they will have extreme difficulty in finding a job. “A Bureau of Justice Statistics (BJS) Study found that one-third of federal prisoners released in 2010 did not find employment within four years” (Finlay, 2022, 1). When former prisoners can’t find work, they tend to go back to what they previously did to earn quick cash. That leaves a scary temptation for inmates to commit the same crimes in order to provide for themselves. In summary of this topic, a reform program in order to be successful needs to have a way for the former prisoners to earn a living outside of the justice system. Once they are released we can’t just expect them to go get a job like nothing happened especially if they have a felony conviction or have a drug related charge.
+                </p>
+                <div>
+                    <p>
+                        <p className='references'>
+                            References used:
+                        </p>
+                        <p className='ref-sources'>
+                            Nancy La Vigne (2024) Transforming Correctional Culture and Climate ojp.gov <br />
+                            Avery G Wilks (2019) To curb jail violence, repeat offenders, SC prisons take page from Europe’s playbook  vera.org <br />
+                            Keith Finlay (2022) Great Recession Had Long-Term Economic Impact on People With Felony Convictions, Prison Time census.gov <br />
+                            Bureau of Justice Statistics (2021) Recidivism of Prisoners Released in 34 States in 2012: A 5-Year Follow-Up Period (2012–2017) ojp.gov
+                        </p>
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+export default Recidivism
