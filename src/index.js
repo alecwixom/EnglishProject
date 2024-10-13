79aa2d35da59be2ea89b61739031a12b985aa4a4
Alec Wixom <atwixom@gmail.com>
2024-10-12 22:40:31 -0600

    update to stack

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Recidivism from './Recidivism';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +10,5 @@ root.render(
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    <Recidivism />
   </React.StrictMode>
 );
